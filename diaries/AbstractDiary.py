32c63da496d4190a599c41461a3f77f1888f7aca
Uchiym <k20018kk@aitech.ac.jp>
2021-12-09 15:38:28 +0900

    日記追加

---
+++ diaries/AbstractDiary.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractclassmethod, abstractmethod
+class AbstractDiary(ABC):
+    @abstractmethod
+    def get_date(sell):
+        pass
+    @abstractmethod
+    def get_summary(self):
+        pass
+    @abstractmethod
+    def get_author(self):
+        pass
